b098e14925023c3e2685731f13ed59f1079f427f
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-01-09 00:28:44 +0900

    #1 PCサイズ/detail サムネクリック完成

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,12 @@
-const swiper = new Swiper(".swiper", {
+const thmbneilSwiper = new Swiper(".swiper-thmbneil", {
+  spaceBetween: 10,
+  slidesPerView: 5
+});
+
+const swiper = new Swiper(".swiper-main", {
+  thumbs: {
+    swiper: thmbneilSwiper
+  },
   pagination: {
     el: ".swiper-pagination",
   },
