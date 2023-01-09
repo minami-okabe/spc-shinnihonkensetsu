d3832b7350c69572e0faf4ef29f36e39d3f5acb7
Minami Okabe <110700776+minami-okabe@users.noreply.github.com>
2023-01-09 15:36:22 +0900

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,21 @@
-const swiper = new Swiper(".swiper", {
+// const swiper = new Swiper(".swiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+const thmbneilSwiper = new Swiper(".swiper-thmbneil", {
+  spaceBetween: 10,
+  slidesPerView: 5
+});
+
+const swiper = new Swiper(".swiper-main", {
+  thumbs: {
+    swiper: thmbneilSwiper
+  },
   pagination: {
     el: ".swiper-pagination",
   },
